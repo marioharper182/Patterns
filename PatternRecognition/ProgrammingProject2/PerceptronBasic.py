64a1b1e09237869eea8d82f2dd93b71b12763cd2
unknown <marioharper182@gmail.com>
2015-12-01 19:04:01 -0500

    Added the working perceptron class, moved some initialization parameters in the testClassifier.py.

---
+++ PatternRecognition/ProgrammingProject2/PerceptronBasic.py
@@ -0,0 +1,54 @@
+__author__ = 'Mario'
+
+from pylab import rand,plot,show,norm
+
+class Perceptron:
+    def __init__(self, elements):
+        """ perceptron initialization """
+        self.w = rand(elements)*2-1 # weights
+        self.wLength = elements
+        self.learningRate = 0.1
+
+    def response(self,x):
+        """ perceptron output """
+        y = 0
+        for i in range(len(x)):
+            y += x[i]*self.w[i]
+        if y >= 0:
+            return 1
+        else:
+            return -1
+
+    def updateWeights(self,x,iterError):
+        """
+        updates the weights status, w at time t+1 is
+           w(t+1) = w(t) + learningRate*(d-r)*x
+        where d is desired output and r the perceptron response
+        iterError is (d-r)
+        """
+        for i in range(self.wLength):
+            self.w[i] += self.learningRate*iterError*x[i]
+
+    def train(self,data,index):
+        """
+        trains all the vector in data.
+        Every vector in data must have three elements,
+        the third element (x[2]) must be the label (desired output)
+        """
+        learned = False
+        iteration = 0
+        size = len(data)
+        while not learned or iteration <= 1000:
+            globalError = 0.0
+            for i in range(size): # for each sample
+                r = self.response(data[i])
+                if index[i] != r: # if we have a wrong response
+                    iterError = index[i] - r # desired response - actual response
+                    self.updateWeights(data[i],iterError)
+                    globalError += abs(iterError)
+                iteration += 1
+                if globalError == 0.0 or iteration >= 100: # stop criteria
+                    print 'iterations',iteration
+                    learned = True # stop learning
+
+Perceptron(3)
--- PatternRecognition/ProgrammingProject2/testClassifier.py
+++ PatternRecognition/ProgrammingProject2/testClassifier.py
@@ -7,6 +7,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.datasets import load_digits
 import pylab as pl
+from PerceptronBasic import Perceptron
 
 
 digits = load_digits()
@@ -15,9 +16,18 @@ digits = load_digits()
 # pl.matshow(digits.images[2])
 # pl.show()
 
-wineVariables, wineIndex = digits.data, digits.target
-digitClassifier = OneVsRestClassifier(LinearSVC(random_state=0)).fit(wineVariables,wineIndex).predict(wineVariables)
-
+digitVariables, digitsIndex = digits.data, digits.target
+digitClassifier = OneVsRestClassifier(LinearSVC(random_state=0)).fit(digitVariables,digitsIndex).predict(digitVariables)
+digitIndexClass0 = [1 if i == 0 else 0 for i in digitsIndex]
+digitIndexClass1 = [1 if i == 1 else 0 for i in digitsIndex]
+digitIndexClass2 = [1 if i == 2 else 0 for i in digitsIndex]
+digitIndexClass3 = [1 if i == 3 else 0 for i in digitsIndex]
+digitIndexClass4 = [1 if i == 4 else 0 for i in digitsIndex]
+digitIndexClass5 = [1 if i == 5 else 0 for i in digitsIndex]
+digitIndexClass6 = [1 if i == 6 else 0 for i in digitsIndex]
+digitIndexClass7 = [1 if i == 7 else 0 for i in digitsIndex]
+digitIndexClass8 = [1 if i == 8 else 0 for i in digitsIndex]
+digitIndexClass9 = [1 if i == 9 else 0 for i in digitsIndex]
 
 # This loads and processes the wine data set into the OneVsRest Classifier
 dataTraining = pd.read_table('./Data/wine_uci_train.txt',delim_whitespace=True, header=None)
@@ -26,13 +36,23 @@ dataTest = pd.read_table('./Data/wine_uci_test.txt',delim_whitespace=True, heade
 wineMax = np.max(dataTraining[13])
 wineMin = np.min(dataTraining[13])
 dataTraining[13] = (dataTraining[13] - wineMin)/(wineMax-wineMin)
+dataTest[13] = (dataTest[13] - wineMin)/(wineMax-wineMin)
 # Creating the classifier variables as a matrix, as well as the classification identifier
-matrix = dataTraining.as_matrix()
-index = [i[0] for i in matrix]
-matrix = [np.delete(i, 0) for i in matrix]
+trainMatrix = dataTraining.as_matrix()
+index = [i[0] for i in trainMatrix]
+trainMatrix = [np.delete(i, 0) for i in trainMatrix]
+# Create the test Matrix
+testMatrix = dataTest.as_matrix()
+indexTest = [i[0] for i in testMatrix]
+testMatrix = [np.delete(i, 0) for i in testMatrix]
 
-wineClassifier = OneVsRestClassifier(LinearSVC(random_state=0)).fit(matrix,index).predict(matrix)
+wineClassifier = OneVsRestClassifier(LinearSVC(random_state=0)).fit(trainMatrix,index).predict(trainMatrix)
 
+indexClass1 = [1 if i == 1 else 0 for i in index]
+indexClass2 = [1 if i == 2 else 0 for i in index]
+indexClass3 = [1 if i == 3 else 0 for i in index]
+winePerceptronTrainer = Perceptron(13)
+winePerceptronTrainer.train(trainMatrix, indexClass1)
 
 
 print "Finished"
